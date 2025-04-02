1a842f4758b6a26b4fdd0289a12b3d999e14a2c6
shenyuan <2497822530@qq.com>
2025-04-02 14:51:03 +0800

    修改日志为logx2

--- backed/easypost.go
+++ backed/easypost.go
@@ -11,6 +11,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"gorm.io/gorm"
